fc0595625921ac6a877d66c58d6f4cc46c20781b
binhkid2 <binhkid2@gmail.com>
2023-05-11 13:55:06 +0700

    env dynamic server

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,10 @@
+
+import { error } from '@sveltejs/kit';
+import { env } from '$env/dynamic/private';
+/** @type {import('./$types').PageLoad} */
+export async function load({ fetch }) {
+    const res = await fetch(`${env.SHEET}`);
+    const products = await res.json();
+   
+    return { products };
+}
